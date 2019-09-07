136a3e9963889510d5d4d97b84c485d331b98401
Anteneh Bedilu <antenehdev@gmail.com>
2019-09-07 01:59:46 -0700

    Splash_Screen

--- Quads/Quads/Quads.Android/MainActivity.cs
+++ Quads/Quads/Quads.Android/MainActivity.cs
@@ -9,11 +9,12 @@ using Android.OS;
 
 namespace Quads.Droid
 {
-    [Activity(Label = "Quads", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Quads", Icon = "@mipmap/icon", Theme = "@style/Theme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
+            SetTheme(Resource.Style.MainTheme);
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
